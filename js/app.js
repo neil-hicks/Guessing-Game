db2fe4534d24562718ab2edc8a42a2ddfa8a84e8
Patrick Hicks <patrick.n.hicks@gmail.com>
2022-08-16 16:30:38 -0500

    Fixed lower case issue

--- js/app.js
+++ js/app.js
@@ -4,31 +4,28 @@ let userName = prompt('What\'s your name?'); {
   alert('Hi, ' + userName + ', and welcome.');
 }
 
-let numChildren = prompt('Do I have three kids?  Please answer yes or no.').toLowerCase();
+let numChildren = prompt('Do I have three kids? Please answer yes or no.').toLowerCase();
 console.log(numChildren);
 
 if (numChildren === null) {
-  prompt('Do I have three kids?').toLowerCase();
+  numChildren = prompt('Do I have three kids?');
 }
-
-if (numChildren === 'No') {
+if (numChildren === 'no') {
   alert('Correct, I have two!');
 } else {
   alert('Incorrect.');
 }
-
 let animals = prompt('Do I have dogs?').toLowerCase();
 console.log(animals);
 
 if (animals === null) {
   prompt('Do I have dogs?').toLowerCase();
 }
-
-if (animals === 'Yes') {
+if (animals === 'yes') {
   alert('Correct, I have two!');
-} /* else {
+} else {
   alert('That\'s not right.');
-} */
+}
 
 
 
