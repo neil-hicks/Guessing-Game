7bc99d7f5274b9bfa03eab22d3f0ca53b3d326c0
Patrick Hicks <patrick.n.hicks@gmail.com>
2022-08-16 16:43:31 -0500

    Fixed questions

--- js/app.js
+++ js/app.js
@@ -27,5 +27,40 @@ if (animals === 'yes') {
   alert('That\'s not right.');
 }
 
+let city = prompt('Do I live in Austin?').toLowerCase();
+console.log(city);
 
+if (city === null) {
+  prompt('Do I live in Austin?').toLowerCase();
+}
+if (city === 'yes') {
+  alert('Correct!');
+} else {
+  alert('Of course I do.');
+}
 
+let job = prompt('Was I in the Air Force?').toLowerCase();
+console.log(job);
+
+if (job === null) {
+  prompt('Was I in the Air Force?').toLowerCase();
+}
+if (job === 'yes') {
+  alert('Yep!');
+} else {
+  alert('That\'s not right.');
+}
+
+let school = prompt('Was I a high school teacher?').toLowerCase();
+console.log(job);
+
+if (school === null) {
+  prompt('Was I a high school teacher?').toLowerCase();
+}
+if (school === 'no') {
+  alert('Right-o!');
+} else {
+  alert('Nope.');
+}
+
+alert('Thanks for playing, ' + userName + '.');
