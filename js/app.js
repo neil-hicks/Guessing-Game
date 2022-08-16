ed6704fedca7b02e09fea33055113f61822983bf
Patrick Hicks <patrick.n.hicks@gmail.com>
2022-08-16 16:05:10 -0500

    Adding questions to page

--- js/app.js
+++ js/app.js
@@ -1,5 +1,34 @@
 'use strict';
 
-let userName = prompt('What\'s your name?')();
+let userName = prompt('What\'s your name?'); {
+  alert('Hi, ' + userName + ', and welcome.');
+}
 
-alert('Hi, ' + userName + ',  and welcome.');
+let numChildren = prompt('Do I have three kids?  Please answer yes or no.').toLowerCase();
+console.log(numChildren);
+
+if (numChildren === null) {
+  prompt('Do I have three kids?').toLowerCase();
+}
+
+if (numChildren === 'No') {
+  alert('Correct, I have two!');
+} else {
+  alert('Incorrect.');
+}
+
+let animals = prompt('Do I have dogs?').toLowerCase();
+console.log(animals);
+
+if (animals === null) {
+  prompt('Do I have dogs?').toLowerCase();
+}
+
+if (animals === 'Yes') {
+  alert('Correct, I have two!');
+} /* else {
+  alert('That\'s not right.');
+} */
+
+
+
