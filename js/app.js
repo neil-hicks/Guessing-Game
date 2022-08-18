d597251e50b01cbd8422c7386a29594a250cdff8
Patrick Hicks <patrick.n.hicks@gmail.com>
2022-08-18 15:07:02 -0500

    fixed format and added console.logs

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 /* eslint-disable indent */
 'use strict';
+let correctAnswers = 0;
 
 let userName = prompt('What\'s your name?'); {
   alert('Hi, ' + userName + ', and welcome.');
@@ -27,7 +28,9 @@ for (let i = 0; i < questions.length; i++) {
 
     for (let answer of answers[i]) {
       if (response.toLowerCase() === answer) {
+        //console.log('Correct!')
         alert('Correct!');
+        correctAnswers++;
         correct = true;
         break;
       }
@@ -45,7 +48,9 @@ for (let guessNum = 0; guessNum < 4; guessNum++) {
   let userNum = parseInt(prompt('Guess a number between 1 and 20: '));
 
   if (userNum === randomNum) {
+    //console.log('Correct!')
     alert('Nice guess! You\'re right.');
+    correctAnswers++;
     break;
   } else if (userNum > randomNum) {
     alert('Too high.');
@@ -57,86 +62,19 @@ for (let guessNum = 0; guessNum < 4; guessNum++) {
 
 let statesLived = ['virginia', 'florida', 'new mexico', 'arizona', 'texas',];
 
-for (let i = 0; i < 7; i++) {
+for (let i = 0; i < 7; i++) { //change to while loop
     let stateGuessed = prompt('Can you guess a state I\'ve lived in?');
-    for (let j = 0; j < statesLived.length; j++) {
+    for (let j = 0; j <statesLived.length; j++) {
 
     if (stateGuessed.toLowerCase() === statesLived[j]) {
+      //console.log('Correct!')
       alert('Good guess!');
-      return;
+      correctAnswers++;
+      break;
+      } else {
+        alert('Try again!');
     }
+  }
 }
-    alert('Try again!');
-}
+alert('Thanks for playing, ' + userName + '.  You got ' + correctAnswers + ' right.');
 
-
-/* let numChildren = prompt('Do I have three kids? Please answer yes or no.').toLowerCase();
-console.log(numChildren);
-
-if (numChildren === null) {
-  numChildren = prompt('Do I have three kids?');
-}
-if (numChildren === 'no' || numChildren === 'n') {
-  //console.log('Correct, I have two!');
-  alert('Correct, I have two!');
-} else {
-  //console.log('Nope.');
-  alert('Incorrect.');
-}
-let animals = prompt('Do I have dogs?').toLowerCase();
-console.log(animals);
-
-if (animals === null) {
-  prompt('Do I have dogs?').toLowerCase();
-}
-if (animals === 'yes' || animals ==='y') {
-  //console.log('That's right.');
-  alert('Correct, I have two!');
-} else {
-  //console.log('Nope.');
-  alert('That\'s not right.');
-}
-
-let city = prompt('Do I live in Austin?').toLowerCase();
-console.log(city);
-
-if (city === null) {
-  prompt('Do I live in Austin?').toLowerCase();
-}
-if (city === 'yes' || city === 'y') {
-  //console.log('That's right.');
-  alert('Correct!');
-} else {
-  //console.log('No, that's not right.');
-  alert('Of course I do.');
-}
-
-let job = prompt('Was I in the Air Force?').toLowerCase();
-console.log(job);
-
-if (job === null) {
-  prompt('Was I in the Air Force?').toLowerCase();
-}
-if (job === 'yes' || job === 'y') {
-  //console.log('Yep!');
-  alert('Yep!');
-} else {
-  //console.log('No, that's not right.');
-  alert('That\'s not right.');
-}
-
-let school = prompt('Was I a high school teacher?').toLowerCase();
-console.log(job);
-
-if (school === null) {
-  prompt('Was I a high school teacher?').toLowerCase();
-}
-if (school === 'no' || school === 'n') {
-  //console.log('That's right.');
-  alert('Right-o!');
-} else {
-  //console.log('Nope.')
-  alert('Nope.');
-}
-*/
-alert('Thanks for playing, ' + userName + '.');
