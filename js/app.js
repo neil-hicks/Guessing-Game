c6c2c1805943f2794dd34f394fb9712e6bcf1d53
Patrick Hicks <patrick.n.hicks@gmail.com>
2022-08-19 10:00:06 -0500

    resolved conflict

--- js/app.js
+++ js/app.js
@@ -1,64 +1,45 @@
 /* eslint-disable indent */
-<<<<<<< HEAD
 'use strict';
-=======
-"use strict";
 let correctAnswers = 0;
->>>>>>> refs/remotes/origin/main
 
-let userName = prompt("What's your name?");
+let userName = prompt('What\'s your name?');
 {
-  alert("Hi, " + userName + ", and welcome.");
+  alert('Hi, ' + userName + ', and welcome.');
 }
 //let correctAnswers = 0;
 
 function quiz() {
   let questions = [
-    "Do I have three children?",
-    "Do I have dogs?",
-    "Do I live in Austin?",
-    "Was I in the Air Force?",
-    "Did I work as a high school teacher?",
+    'Do I have three children?',
+    'Do I have dogs?',
+    'Do I live in Austin?',
+    'Was I in the Air Force?',
+    'Did I work as a high school teacher?',
   ];
   let answers = [
-    ["no", "n"],
-    ["yes", "y"],
-    ["yes", "y"],
-    ["yes", "y"],
-    ["no", "n"],
+    ['no', 'n'],
+    ['yes', 'y'],
+    ['yes', 'y'],
+    ['yes', 'y'],
+    ['no', 'n'],
   ];
 
-<<<<<<< HEAD
-for (let i = 0; i < questions.length; i++) {
-  let response = prompt(questions[i]);
-  let correct = false;
-  if (typeof(answers[i]) !== 'string') {
-
-    for (let answer of answers[i]) {
-      if (response.toLowerCase() === answer) {
-        //console.log('Correct!')
-        alert('Correct!');
-        //correctAnswers++;
-        correct = true;
-        break;
-=======
   for (let i = 0; i < questions.length; i++) {
     let response = prompt(questions[i]);
     let correct = false;
-    if (typeof answers[i] !== "string") {
+    if (typeof answers[i] !== 'string') {
       for (let answer of answers[i]) {
         if (response.toLowerCase() === answer) {
           //console.log('Correct!')
-          alert("Correct!");
+          alert('Correct!');
           correctAnswers++;
           correct = true;
           break;
         }
->>>>>>> refs/remotes/origin/main
       }
     }
     if (correct === false) {
-      alert("Incorrect!");
+      alert('Incorrect!');
     }
   }
 }
@@ -69,38 +50,22 @@ function number() {
   console.log(randomNum);
 
   for (let guessNum = 0; guessNum < 4; guessNum++) {
-    let userNum = parseInt(prompt("Guess a number between 1 and 20: "));
+    let userNum = parseInt(prompt('Guess a number between 1 and 20: '));
 
-<<<<<<< HEAD
-  if (userNum === randomNum) {
-    //console.log('Correct!')
-    alert('Nice guess! You\'re right.');
-    //correctAnswers++;
-    break;
-  } else if (userNum > randomNum) {
-    alert('Too high.');
-  } else {
-    alert('Too low!');
-=======
     if (userNum === randomNum) {
       //console.log('Correct!')
-      alert("Nice guess! You're right.");
+      alert('Nice guess! You\'re right.');
       correctAnswers++;
       break;
     } else if (userNum > randomNum) {
-      alert("Too high.");
+      alert('Too high.');
     } else {
-      alert("Too low!");
+      alert('Too low!');
     }
->>>>>>> refs/remotes/origin/main
   }
 
 }
-<<<<<<< HEAD
-states();
-=======
 number();
->>>>>>> refs/remotes/origin/main
 
 
   let statesLived = ['virginia', 'florida', 'new mexico', 'arizona', 'texas',];
@@ -121,14 +86,8 @@ while (lived === false) {
     }
   }
 }
-<<<<<<< HEAD
-
-// alert('Thanks for playing, ' + userName + '.  You got ' + correctAnswers + ' right.');
-
 }
-=======
-*/
+states();
 alert(
-  "Thanks for playing, " + userName + ".  You got " + correctAnswers + " right."
+  'Thanks for playing, ' + userName + '.  You got ' + correctAnswers + ' right.'
 );
->>>>>>> refs/remotes/origin/main
